2b425f8407fc462d31794be7d1bac458aa11390f
Cesar Romero <cesareromerog88@gmail.com>
2018-01-17 16:02:27 -0400

    Migraciones y Modelos del RQ4

---
+++ app/Http/Controllers/WallsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Wall;
+use Illuminate\Http\Request;
+
+class WallsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Wall  $wall
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Wall $wall)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Wall  $wall
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Wall $wall)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Wall  $wall
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Wall $wall)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Wall  $wall
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Wall $wall)
+    {
+        //
+    }
+}
