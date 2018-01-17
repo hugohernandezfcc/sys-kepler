44f91f7b5050a78cd12e30dd71491e7661a1a6b4
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-01-17 01:22:36 -0600

    agregando campo de tipo en usuario

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -81,7 +81,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    Registrar información
                                 </button>
                             </div>
                         </div>
