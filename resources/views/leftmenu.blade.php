747f1ee378806c89025333ac75d0586a5b2f5eeb
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-01-17 14:33:51 -0600

    modificando los menus

--- resources/views/leftmenu.blade.php
+++ resources/views/leftmenu.blade.php
@@ -3,11 +3,13 @@
         <div class="sidebar-collapse">
             <ul class="nav metismenu" id="side-menu">
                 <li class="nav-header">
-                    <div class="dropdown profile-element"> <span>
-                        <img alt="image" class="img-circle" src="img/profile_small.jpg" />
-                         </span>
+                    <div class="dropdown profile-element"> 
+                        <span>
+                            <img alt="image" class="img-circle" src="img/profile_small.jpg" />
+                        </span>
                         <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                        <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">Sys Kepler</strong>
+                        <span class="clear"> 
+                            <span class="block m-t-xs"> <strong class="font-bold">Sys Kepler</strong>
                          </span> <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> </span> </a>
                         <ul class="dropdown-menu animated fadeInRight m-t-xs">
                             <li><a href="profile.html">Profile</a></li>
@@ -21,7 +23,7 @@
                         Kepler
                     </div>
                 </li>
-                <li class="active">
+                <!-- <li class="active">
                     <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboards</span> <span class="fa arrow"></span></a>
                     <ul class="nav nav-second-level">
                         <li class="active"><a href="index.html">Dashboard v.1</a></li>
@@ -30,11 +32,34 @@
                         <li><a href="dashboard_4_1.html">Dashboard v.4</a></li>
                         <li><a href="dashboard_5.html">Dashboard v.5 </a></li>
                     </ul>
+                </li> -->
+                @if (1)
+                <li><a href="layouts.html"><i class="fa fa-diamond"></i> <span class="nav-label">Grupos</span></a></li>
+                <li><a href="layouts.html"><i class="fa fa-diamond"></i> <span class="nav-label">Ciclos escolares</span></a></li>
+                <li><a href="layouts.html"><i class="fa fa-diamond"></i> <span class="nav-label">Areas</span></a></li>
+                <li>
+                    <a href="layouts.html"><i class="fa fa-diamond"></i> <span class="nav-label">Asignaturas</span></a>
+                    <ul class="nav nav-second-level">
+                        <li class="active"><a href="index.html">Tareas</a></li>
+                        <li><a href="dashboard_2.html">Pase de lista</a></li>
+                        <li><a href="dashboard_3.html">Modulos</a></li>
+                        <li><a href="dashboard_4_1.html">Examenes</a></li>
+                    </ul>
                 </li>
+                @elseif (0)
                 <li>
                     <a href="layouts.html"><i class="fa fa-diamond"></i> <span class="nav-label">Layouts</span></a>
                 </li>
                 <li>
+                    <a href="layouts.html"><i class="fa fa-diamond"></i> <span class="nav-label">Layouts</span></a>
+                </li>
+                @else
+                <li>
+                    <a href="layouts.html"><i class="fa fa-diamond"></i> <span class="nav-label">Layouts</span></a>
+                </li>
+                @endif
+
+                <!-- <li>
                     <a href="mailbox.html"><i class="fa fa-envelope"></i> <span class="nav-label">Mailbox </span><span class="label label-warning pull-right">16/24</span></a>
                     <ul class="nav nav-second-level collapse">
                         <li><a href="mailbox.html">Inbox</a></li>
@@ -85,7 +110,7 @@
                 </li>
                 <li class="special_link">
                     <a href="package.html"><i class="fa fa-database"></i> <span class="nav-label">Package</span></a>
-                </li>
+                </li> -->
             </ul>
 
         </div>
