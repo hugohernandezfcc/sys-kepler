a83a174edf61b0a51ef3e2f00379f54a610397a4
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-01-18 09:57:54 -0600

    sadfs

--- resources/views/subjects.blade.php
+++ resources/views/subjects.blade.php
@@ -89,23 +89,12 @@
                     <div class="col-lg-12">
                     <div class="ibox float-e-margins">
                         <div class="ibox-title">
-                            <h5>Basic Data Tables example with responsive plugin</h5>
+                            <h5>Lista de asignaturas</h5>
                             <div class="ibox-tools">
                                 <a class="collapse-link">
                                     <i class="fa fa-chevron-up"></i>
                                 </a>
-                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                                    <i class="fa fa-wrench"></i>
-                                </a>
-                                <ul class="dropdown-menu dropdown-user">
-                                    <li><a href="#">Config option 1</a>
-                                    </li>
-                                    <li><a href="#">Config option 2</a>
-                                    </li>
-                                </ul>
-                                <a class="close-link">
-                                    <i class="fa fa-times"></i>
-                                </a>
+                                
                             </div>
                         </div>
                         <div class="ibox-content">
