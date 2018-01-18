bb4efacd49de1da3e381f8c288b1f618106ce077
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-01-18 11:13:31 -0600

    d

--- resources/views/home_master.blade.php
+++ resources/views/home_master.blade.php
@@ -11,8 +11,8 @@
                                 <i class="fa fa-trophy fa-5x"></i>
                             </div>
                             <div class="col-xs-8 text-right">
-                                <span> Today income </span>
-                                <h2 class="font-bold">$ 4,232</h2>
+                                <span> ROI </span>
+                                <h2 class="font-bold">$ 10,730</h2>
                             </div>
                         </div>
                 </div>
@@ -21,11 +21,11 @@
                 <div class="widget style1 navy-bg">
                     <div class="row">
                         <div class="col-xs-4">
-                            <i class="fa fa-cloud fa-5x"></i>
+                            <i class="fa fa-fa-group fa-5x"></i>
                         </div>
                         <div class="col-xs-8 text-right">
-                            <span> Today degrees </span>
-                            <h2 class="font-bold">26'C</h2>
+                            <span> Grupos asignados </span>
+                            <h2 class="font-bold">10</h2>
                         </div>
                     </div>
                 </div>
@@ -37,8 +37,8 @@
                             <i class="fa fa-envelope-o fa-5x"></i>
                         </div>
                         <div class="col-xs-8 text-right">
-                            <span> New messages </span>
-                            <h2 class="font-bold">260</h2>
+                            <span> Nuevos mensajes </span>
+                            <h2 class="font-bold">17</h2>
                         </div>
                     </div>
                 </div>
@@ -47,10 +47,10 @@
                 <div class="widget style1 yellow-bg">
                     <div class="row">
                         <div class="col-xs-4">
-                            <i class="fa fa-music fa-5x"></i>
+                            <i class="fa fa-wechat fa-5x"></i>
                         </div>
                         <div class="col-xs-8 text-right">
-                            <span> New albums </span>
+                            <span> Nuevas dudas </span>
                             <h2 class="font-bold">12</h2>
                         </div>
                     </div>
@@ -66,23 +66,22 @@
                         <div class="widget-head-color-box navy-bg p-lg text-center">
                             <div class="m-b-md">
                             <h2 class="font-bold no-margins">
-                                Alex Smith
+                                {{Auth::user()->name}}
                             </h2>
-                                <small>Founder of Groupeq</small>
+                                <small>Docente de español</small>
                             </div>
-                            <img src="img/a4.jpg" class="img-circle circle-border m-b-md" alt="profile">
+                            <img src="{{ asset('inspinia/img/a4.jpg')}}" class="img-circle circle-border m-b-md" alt="profile">
                             <div>
-                                <span>100 Tweets</span> |
-                                <span>350 Following</span> |
-                                <span>610 Followers</span>
+                                <span>100 Respuestas</span> |
+                                <span>350 Seguidores</span> |
+                                <span>14 Artículos</span>
                             </div>
                         </div>
                         <div class="widget-text-box">
-                            <h4 class="media-heading">Alex Smith</h4>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <h4 class="media-heading">{{Auth::user()->name}}</h4>
+                            <p>Docente con más de 10 años de experiencia en la docencía</p>
                             <div class="text-right">
-                                <a class="btn btn-xs btn-white"><i class="fa fa-thumbs-up"></i> Like </a>
-                                <a class="btn btn-xs btn-primary"><i class="fa fa-heart"></i> Love</a>
+                                
                             </div>
                         </div>
                 </div>
@@ -168,6 +167,6 @@
                     </div>
             </div>
 
-        
+
 
 @endsection
