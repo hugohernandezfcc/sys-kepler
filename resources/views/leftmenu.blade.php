3229921a2e0f1c49d671bbbc706070a91d220373
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-01-18 09:20:45 -0600

    e

--- resources/views/leftmenu.blade.php
+++ resources/views/leftmenu.blade.php
@@ -39,23 +39,23 @@
                 <li><a href="/cyclescontrol"><i class="fa fa-university"></i> <span class="nav-label">Ciclos escolares</span></a></li>
                 <li><a href="/areas"><i class="fa fa-subscript"></i> <span class="nav-label">Areas</span></a></li>
                 <li>
-                    <a href="layouts.html"><i class="fa fa-lis-ul"></i> <span class="nav-label">Contenidos</span><span class="fa fa-ellipsis-v"></span></a>
+                    <a href="layouts.html"><i class="fa fa-ellipsis-v"></i> <span class="nav-label">Contenidos</span><span class="fa arrows"></span></a>
                     <ul class="nav nav-second-level">
                         <li ><a href="/subjects"><i class="fa fa-graduation-cap"></i> Asignaturas</a></li>
-                        <li ><a href="/task"><i class="fa fa-task"></i> Tareas</a></li>
+                        <li ><a href="/task"><i class="fa fa-tasks"></i> Tareas</a></li>
                         <li><a href="/list"><i class="fa fa-list"></i>  Pase de lista</a></li>
                         <li><a href="/modules"><i class="fa fa-archive"></i> Modulos</a></li>
                         <li><a href="/test"><i class="fa fa-flask"></i> Examenes</a></li>
                     </ul>
                 </li>
-                @elseif (0)
+                @elseif (Auth::user()->type == "student")
                 <li>
                     <a href="layouts.html"><i class="fa fa-diamond"></i> <span class="nav-label">Layouts</span></a>
                 </li>
                 <li>
                     <a href="layouts.html"><i class="fa fa-diamond"></i> <span class="nav-label">Layouts</span></a>
                 </li>
-                @else
+                @elseif (Auth::user()->type == "a")
                 <li>
                     <a href="layouts.html"><i class="fa fa-diamond"></i> <span class="nav-label">Layouts</span></a>
                 </li>
