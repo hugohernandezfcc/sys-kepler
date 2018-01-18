b7ec490445fe5e08b4498761cfa6a4103ef7d1b9
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-01-17 22:56:53 -0600

    cambiando el footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,8 +1,10 @@
-<div class="footer">
-    <div class="pull-right">
-        10GB of <strong>250GB</strong> Free.
-    </div>
-    <div>
-        <strong>Copyright</strong> Kepler &copy; 2017-2018
-    </div>
-</div>
+<footer>
+	<div class="footer">
+	    <div class="pull-right">
+	        10GB of <strong>250GB</strong> Free.
+	    </div>
+	    <div>
+	        <strong>Copyright</strong> Kepler &copy; 2017-2018
+	    </div>
+	</div>
+</footer>
