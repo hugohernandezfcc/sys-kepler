7662ee02eb4c8cb094e2331dad44ba0525452820
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-01-17 22:36:22 -0600

    agregando grafico

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,8 +56,150 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-title">
+                        <h5>Ingresos por ciclo semestral</h5>
+                        <div class="pull-right">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-xs btn-white active">Ciclo actual</button>
+                                <button type="button" class="btn btn-xs btn-white">Anual</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ibox-content">
+                        <div class="row">
+                        <div class="col-lg-9">
+                            <div class="flot-chart">
+                                <div class="flot-chart-content" id="flot-dashboard-chart"></div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <ul class="stat-list">
+                                <li>
+                                    <h2 class="no-margins">2,346</h2>
+                                    <small>Inversión</small>
+                                    <div class="stat-percent">48% <i class="fa fa-level-up text-navy"></i></div>
+                                    <div class="progress progress-mini">
+                                        <div style="width: 48%;" class="progress-bar"></div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <h2 class="no-margins ">4,422</h2>
+                                    <small>Total de ingresos</small>
+                                    <div class="stat-percent">60% <i class="fa fa-level-down text-navy"></i></div>
+                                    <div class="progress progress-mini">
+                                        <div style="width: 60%;" class="progress-bar"></div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <h2 class="no-margins ">9,180</h2>
+                                    <small>Ganancias</small>
+                                    <div class="stat-percent">22% <i class="fa fa-bolt text-navy"></i></div>
+                                    <div class="progress progress-mini">
+                                        <div style="width: 22%;" class="progress-bar"></div>
+                                    </div>
+                                </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
 
-        
-        
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
+
+    <script type="text/javascript">
+        var dataset = [
+                {
+                    label: "Ganancias",
+                    data: data3,
+                    color: "#1ab394",
+                    bars: {
+                        show: true,
+                        align: "center",
+                        barWidth: 24 * 60 * 60 * 600,
+                        lineWidth:0
+                    }
+
+                }, {
+                    label: "Ingresos",
+                    data: data2,
+                    yaxis: 2,
+                    color: "#1C84C6",
+                    lines: {
+                        lineWidth:1,
+                            show: true,
+                            fill: true,
+                        fillColor: {
+                            colors: [{
+                                opacity: 0.2
+                            }, {
+                                opacity: 0.4
+                            }]
+                        }
+                    },
+                    splines: {
+                        show: false,
+                        tension: 0.6,
+                        lineWidth: 1,
+                        fill: 0.1
+                    },
+                }
+            ];
+
+
+            var options = {
+                xaxis: {
+                    mode: "time",
+                    tickSize: [3, "day"],
+                    tickLength: 0,
+                    axisLabel: "Date",
+                    axisLabelUseCanvas: true,
+                    axisLabelFontSizePixels: 12,
+                    axisLabelFontFamily: 'Arial',
+                    axisLabelPadding: 10,
+                    color: "#d5d5d5"
+                },
+                yaxes: [{
+                    position: "left",
+                    max: 1070,
+                    color: "#d5d5d5",
+                    axisLabelUseCanvas: true,
+                    axisLabelFontSizePixels: 12,
+                    axisLabelFontFamily: 'Arial',
+                    axisLabelPadding: 3
+                }, {
+                    position: "right",
+                    clolor: "#d5d5d5",
+                    axisLabelUseCanvas: true,
+                    axisLabelFontSizePixels: 12,
+                    axisLabelFontFamily: ' Arial',
+                    axisLabelPadding: 67
+                }
+                ],
+                legend: {
+                    noColumns: 1,
+                    labelBoxBorderColor: "#000000",
+                    position: "nw"
+                },
+                grid: {
+                    hoverable: false,
+                    borderWidth: 0
+                }
+            };
+
+            function gd(year, month, day) {
+                return new Date(year, month - 1, day).getTime();
+            }
+
+            var previousPoint = null, previousLabel = null;
+
+            $.plot($("#flot-dashboard-chart"), dataset, options);
+    </script>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,8 +42,39 @@
     <script src="{{ asset('inspinia/js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
     <script src="{{ asset('inspinia/js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
 
+
+    <!-- Flot -->
+    <script src="{{ asset('inspinia/js/plugins/flot/jquery.flot.js') }}"></script>
+    <script src="{{ asset('inspinia/js/plugins/flot/jquery.flot.tooltip.min.js') }}"></script>
+    <script src="{{ asset('inspinia/js/plugins/flot/jquery.flot.spline.js') }}"></script>
+    <script src="{{ asset('inspinia/js/plugins/flot/jquery.flot.resize.js') }}"></script>
+    <script src="{{ asset('inspinia/js/plugins/flot/jquery.flot.pie.js') }}"></script>
+    <script src="{{ asset('inspinia/js/plugins/flot/jquery.flot.symbol.js') }}"></script>
+    <script src="{{ asset('inspinia/js/plugins/flot/jquery.flot.time.js') }}"></script>
+
+    <!-- Peity -->
+    <script src="{{ asset('inspinia/js/plugins/peity/jquery.peity.min.js') }}"></script>
+    <script src="{{ asset('inspinia/js/demo/peity-demo.js') }}"></script>
+
     <!-- Custom and plugin javascript -->
     <script src="{{ asset('inspinia/js/inspinia.js') }}"></script>
     <script src="{{ asset('inspinia/js/plugins/pace/pace.min.js') }}"></script>
+
+    <!-- jQuery UI -->
+    <script src="{{ asset('inspinia/js/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+
+    <!-- Jvectormap -->
+    <script src="{{ asset('inspinia/js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js')}}"></script>
+    <script src="{{ asset('inspinia/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+
+    <!-- EayPIE -->
+    <script src="{{ asset('inspinia/js/plugins/easypiechart/jquery.easypiechart.js')}}"></script>
+
+    <!-- Sparkline -->
+    <script src="{{ asset('inspinia/js/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
+
+    <!-- Sparkline demo data  -->
+    <script src="{{ asset('inspinia/js/demo/sparkline-demo.js')}}"></script>
+
 </body>
 </html>
