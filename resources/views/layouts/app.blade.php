94d0273da1aee2ce255bea61f9c822552d8a57d4
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-01-18 01:44:43 -0600

    corrigiendo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -193,28 +193,24 @@
 
             $(document).ready(function(){
 
-                if (document.getElementByClassName('dataTables-example')) {
-                    $('.dataTables-example').DataTable({
-                        pageLength: 10,
-                        responsive: true,
-                        dom: '<"html5buttons"B>lTfgitp',
-                        buttons: [
-                            {extend: 'copy'},
-                            {extend: 'csv'},
-                            {extend: 'excel', title: 'Datos_kepler'},
-                            {extend: 'pdf', title: 'Datos_kepler'},
-                            {extend: 'print',
-                                customize: function (win){
-                                        $(win.document.body).addClass('white-bg');
-                                        $(win.document.body).css('font-size', '10px');
-                                        $(win.document.body).find('table').addClass('compact').css('font-size', 'inherit');
-                                }
+                $('.dataTables-example').DataTable({
+                    pageLength: 10,
+                    responsive: true,
+                    dom: '<"html5buttons"B>lTfgitp',
+                    buttons: [
+                        {extend: 'copy'},
+                        {extend: 'csv'},
+                        {extend: 'excel', title: 'Datos_kepler'},
+                        {extend: 'pdf', title: 'Datos_kepler'},
+                        {extend: 'print',
+                            customize: function (win){
+                                    $(win.document.body).addClass('white-bg');
+                                    $(win.document.body).css('font-size', '10px');
+                                    $(win.document.body).find('table').addClass('compact').css('font-size', 'inherit');
                             }
-                        ]
-
-                    });
-                }
-                
+                        }
+                    ]
+                });
             });
 
     </script>
--- resources/views/subjects.blade.php
+++ resources/views/subjects.blade.php
@@ -23,6 +23,7 @@
     
 
     @if($typeView == 'form')
+        <br/>
         <div class="row">
             <div class="col-lg-12">
                 <div class="ibox float-e-margins">
@@ -32,15 +33,6 @@
                             <a class="collapse-link">
                                 <i class="fa fa-chevron-up"></i>
                             </a>
-                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                                <i class="fa fa-wrench"></i>
-                            </a>
-                            <ul class="dropdown-menu dropdown-user">
-                                <li><a href="#">Config option 1</a>
-                                </li>
-                                <li><a href="#">Config option 2</a>
-                                </li>
-                            </ul>
                         </div>
                     </div>
                     <div class="ibox-content">
