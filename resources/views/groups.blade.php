b2f6d291eeeeb934f7c622e83d062421ced3fac9
Cesar Romero <cesareromerog88@gmail.com>
2018-01-26 20:27:12 -0400

    Agregando tab de Miembros al detalle del grupo

--- resources/views/groups.blade.php
+++ resources/views/groups.blade.php
@@ -238,7 +238,7 @@
                             <div class="panel-options">
                                 <ul class="nav nav-tabs">
                                     <li class="active"><a href="#tab-1" data-toggle="tab">Comentarios</a></li>
-                                    <li class=""><a href="#tab-2" data-toggle="tab">Elementos relacionados</a></li>
+                                    <li class=""><a href="#tab-2" data-toggle="tab">Miembros</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -325,198 +325,22 @@
                                     <table class="table table-striped">
                                         <thead>
                                             <tr>
-                                                <th>Status</th>
-                                                <th>Title</th>
-                                                <th>Start Time</th>
-                                                <th>End Time</th>
-                                                <th>Comments</th>
+                                                <th>Nombre</th>
+                                                <th></th>
+                                                <th>Correo</th>
                                             </tr>
                                         </thead>
                                         <tbody>
+                                            @foreach ($record->users as $groupUser)
                                             <tr>
-                                                <td>
-                                                    <span class="label label-primary"><i class="fa fa-check"></i> Completed</span>
-                                                </td>
-                                                <td>
-                                                    Create project in webapp
-                                                </td>
-                                                <td>
-                                                    12.07.2014 10:10:1
-                                                </td>
-                                                <td>
-                                                    14.07.2014 10:16:36
-                                                </td>
-                                                <td>
-                                                    <p class="small">
-                                                        Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable.
-                                                    </p>
-                                                </td>
-
+                                                <td> {{ $groupUser->name }} </td>
+                                                <td class="contact-type"><i class="fa fa-envelope"> </i></td>
+                                                <td> {{ $groupUser->email }} </td>
                                             </tr>
-                                            <tr>
-                                                <td>
-                                                    <span class="label label-primary"><i class="fa fa-check"></i> Accepted</span>
-                                                </td>
-                                                <td>
-                                                    Various versions
-                                                </td>
-                                                <td>
-                                                    12.07.2014 10:10:1
-                                                </td>
-                                                <td>
-                                                    14.07.2014 10:16:36
-                                                </td>
-                                                <td>
-                                                    <p class="small">
-                                                        Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-                                                    </p>
-                                                </td>
-
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <span class="label label-primary"><i class="fa fa-check"></i> Sent</span>
-                                                </td>
-                                                <td>
-                                                    There are many variations
-                                                </td>
-                                                <td>
-                                                    12.07.2014 10:10:1
-                                                </td>
-                                                <td>
-                                                    14.07.2014 10:16:36
-                                                </td>
-                                                <td>
-                                                    <p class="small">
-                                                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which
-                                                    </p>
-                                                </td>
-
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <span class="label label-primary"><i class="fa fa-check"></i> Reported</span>
-                                                </td>
-                                                <td>
-                                                    Latin words
-                                                </td>
-                                                <td>
-                                                    12.07.2014 10:10:1
-                                                </td>
-                                                <td>
-                                                    14.07.2014 10:16:36
-                                                </td>
-                                                <td>
-                                                    <p class="small">
-                                                        Latin words, combined with a handful of model sentence structures
-                                                    </p>
-                                                </td>
-
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <span class="label label-primary"><i class="fa fa-check"></i> Accepted</span>
-                                                </td>
-                                                <td>
-                                                    The generated Lorem
-                                                </td>
-                                                <td>
-                                                    12.07.2014 10:10:1
-                                                </td>
-                                                <td>
-                                                    14.07.2014 10:16:36
-                                                </td>
-                                                <td>
-                                                    <p class="small">
-                                                        The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
-                                                    </p>
-                                                </td>
-
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <span class="label label-primary"><i class="fa fa-check"></i> Sent</span>
-                                                </td>
-                                                <td>
-                                                    The first line
-                                                </td>
-                                                <td>
-                                                    12.07.2014 10:10:1
-                                                </td>
-                                                <td>
-                                                    14.07.2014 10:16:36
-                                                </td>
-                                                <td>
-                                                    <p class="small">
-                                                        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-                                                    </p>
-                                                </td>
-
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <span class="label label-primary"><i class="fa fa-check"></i> Reported</span>
-                                                </td>
-                                                <td>
-                                                    The standard chunk
-                                                </td>
-                                                <td>
-                                                    12.07.2014 10:10:1
-                                                </td>
-                                                <td>
-                                                    14.07.2014 10:16:36
-                                                </td>
-                                                <td>
-                                                    <p class="small">
-                                                        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
-                                                    </p>
-                                                </td>
-
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <span class="label label-primary"><i class="fa fa-check"></i> Completed</span>
-                                                </td>
-                                                <td>
-                                                    Lorem Ipsum is that
-                                                </td>
-                                                <td>
-                                                    12.07.2014 10:10:1
-                                                </td>
-                                                <td>
-                                                    14.07.2014 10:16:36
-                                                </td>
-                                                <td>
-                                                    <p class="small">
-                                                        Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable.
-                                                    </p>
-                                                </td>
-
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <span class="label label-primary"><i class="fa fa-check"></i> Sent</span>
-                                                </td>
-                                                <td>
-                                                    Contrary to popular
-                                                </td>
-                                                <td>
-                                                    12.07.2014 10:10:1
-                                                </td>
-                                                <td>
-                                                    14.07.2014 10:16:36
-                                                </td>
-                                                <td>
-                                                    <p class="small">
-                                                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical
-                                                    </p>
-                                                </td>
-
-                                            </tr>
+                                            @endforeach
 
                                         </tbody>
                                     </table>
-
                                 </div>
                             </div>
                         </div>
