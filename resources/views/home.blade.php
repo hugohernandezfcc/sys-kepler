3078566b7fa0a0bd394b49c70f988b9374b2b8f8
Cesar Romero <cesareromerog88@gmail.com>
2018-03-05 21:48:30 -0400

    Ajustes en Grupos.- Mostrando cantidades en Home.-

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,10 @@
         <div class="col-lg-3">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <span class="label label-success pull-right">Nuevo</span>
                     <h5>Áreas</h5>
                 </div>
                 <div class="ibox-content">
-                    <h1 class="no-margins">232</h1>
-                    <div class="stat-percent font-bold text-success">98% <i class="fa fa-bolt"></i></div>
+                    <h1 class="no-margins">{{ $records['areas'] }}</h1>
                     <small>Total de áreas activas</small>
                 </div>
             </div>
@@ -19,12 +17,10 @@
         <div class="col-lg-3">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <span class="label label-info pull-right">Nuevo</span>
                     <h5>Asignaturas</h5>
                 </div>
                 <div class="ibox-content">
-                    <h1 class="no-margins">105</h1>
-                    <div class="stat-percent font-bold text-info">20% <i class="fa fa-level-up"></i></div>
+                    <h1 class="no-margins">{{ $records['subjects'] }}</h1>
                     <small>Asignaturas</small>
                 </div>
             </div>
@@ -32,12 +28,10 @@
         <div class="col-lg-3">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <span class="label label-primary pull-right">Nuevo</span>
                     <h5>Alumnos</h5>
                 </div>
                 <div class="ibox-content">
-                    <h1 class="no-margins">106,120</h1>
-                    <div class="stat-percent font-bold text-navy">44% <i class="fa fa-level-up"></i></div>
+                    <h1 class="no-margins">{{ $records['students'] }}</h1>
                     <small>Alumnos activos</small>
                 </div>
             </div>
@@ -45,13 +39,11 @@
         <div class="col-lg-3">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <span class="label label-danger pull-right">Nuevo</span>
-                    <h5>Bajas</h5>
+                    <h5>Grupos</h5>
                 </div>
                 <div class="ibox-content">
-                    <h1 class="no-margins">10</h1>
-                    <div class="stat-percent font-bold text-danger">38% <i class="fa fa-level-down"></i></div>
-                    <small>Alumnos dados de baja</small>
+                    <h1 class="no-margins">{{ $records['groups'] }}</h1>
+                    <small>Grupos</small>
                 </div>
             </div>
         </div>
